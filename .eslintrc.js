c679a97f776552506f84a1c8322603fc551b92db
Salakhutdinov Salavat <salahutdinov.salavat@gmail.com>
2023-02-16 10:10:13 +0300

    chore(eslint): disable `@typescript-eslint/ban-types` rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,5 +28,6 @@ module.exports = {
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/ban-types': 'off',
   },
 };
